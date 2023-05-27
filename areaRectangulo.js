27cb2623a54a0dd5b4430aa12aafd4a3fa1380b2
Fernandez-alber <fernandez.alber@hotmail.com>
2023-05-27 20:01:03 -0300

    Practico N0 finalizado

---
+++ areaRectangulo.js
@@ -0,0 +1,12 @@
+
+let base1 = 5;
+let altura1 = 3;
+
+let base2 = 12.5;
+let altura2 = 5.3;
+
+let areaDeUnRectangulo1 = base1*altura1;
+let areaDeUnRectangulo2 = base2*altura2;
+
+console.log (areaDeUnRectangulo1);
+console.log (areaDeUnRectangulo2);
---
+++ promedio3.js
@@ -0,0 +1,11 @@
+let promedio1= (5+7+9) / 3;
+
+let promedio2= ((-2)+(-3)+(-8));
+
+let promedio3= ((-1.2)+5.3+(-3.3));
+
+console.log (promedio1);
+console.log (promedio2);
+console.log (promedio3);
+
+
